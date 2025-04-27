8e6d33326a1a6e8765f6dddb3d1f20243fe17239
Błażej <wargun364@gmail.com>
2025-04-27 21:05:44 +0200

    morquee finished

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,57 +1,73 @@
 import {motion} from 'motion/react'
+import photoshop from '../assets/images/photoshop.svg'
+import indesign from '../assets/images/indesign.svg'
+import illustrator from '../assets/images/illustrator.svg'
+import autocad from '../assets/images/autocad.png'
+import rhinoceros from '../assets/images/rhinoceros.svg'
+import lumion from '../assets/images/Lumion.svg'
+import revit from '../assets/images/Revit.png'
+import enscape from '../assets/images/enscape.png'
+import sketchup from '../assets/images/sketchup.svg'
 
 export default function Marquee(){
+
     const softwares=[{
-        src:"",
+        src: photoshop,
         name:"Photoshop"
     },
     {
-        src:"",
-        name:"In design"
+        src:indesign,
+        name:"InDesign"
     },
     {
-        src:"",
+        src: illustrator,
         name:"Adobe illustrator"
     },
     {
-        src:"",
+        src: autocad,
         name:"Autocad"
     },
     {
-        src:"",
+        src: rhinoceros,
         name:"Rhinoceros"
     },
     {
-        src:"",
+        src: lumion,
         name:"Lumion"
     },
     {
-        src:"",
+        src: revit,
         name:"Revit"
     },
     {
-        src:"",
+        src: enscape,
         name:"Enscape"
     },
     {
-        src:"",
+        src: sketchup,
         name:"Sketchup"
     },
 ]
 
     return(<>
-    <div className="relative mt-96 "></div>
-        <div className="w-lvw absolute -left-2 -rotate-2 h-[7em] bg-wood-brown"></div>
-        <div className="w-full bg-gray-sky h-[7em] relative overflow-hidden">
-            <motion.div className="flex gap-10 absolute top-1/2 -translate-y-1/2"
-                initial={{x:"-100%"}}
-                animate={{x:"100%"}}
-                transition={{ ease: "linear", duration: 15, repeat: Infinity, repeatType:"loop" }}
+    <div className="relative mt-96"></div>
+        <div className="absolute inset-x-0 -rotate-4 md:-rotate-2 h-[4em] md:h-[7em] bg-wood-brown"></div>
+        <div className="w-full bg-gray-sky h-[4em] md:h-[7em] relative overflow-hidden">
+            <motion.div className={`flex -right-4 gap-10 absolute flex-nowrap w-fit whitespace-nowrap top-1/2 -translate-y-1/2`}
+                initial="off"
+                animate="on"
+                variants={{
+                    off: {right:`0%`,x:"100%"},
+                    on:{right:"100%", x:"0%"}
+                }}
+                transition={{ ease: "linear", duration: 40, repeat: Infinity, repeatType:"loop" }}
             >
-            {softwares.map((sofware,index)=>(
-                <div key={index} className="flex">
-                    <h3 className="text-4xl font-alta text-wood-brown">{sofware.name}</h3>
-                    <img/>
+            {softwares.map((software,index)=>(
+                <div key={index} className="flex items-center gap-10 ">
+                    <h3 className="text-2xl md:text-4xl font-alta text-wood-brown">{software.name}</h3>
+                    <div className="md:w-[4em] w-[2.5em]">
+                        <img  src={software.src} alt={software.name}/>
+                    </div>
                 </div>
             ))}
             </motion.div>
